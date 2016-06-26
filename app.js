eb5771d9b6c018004740dbea1d9a8415da8d782e
jpsaroudakis <jops@outlook.com>
2016-06-25 22:27:08 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+  
+// Create bot and setup server
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', connector.verifyBotFramework(), connector.listen());
+server.listen(process.env.port || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
